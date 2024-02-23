9084703ec644397eaab47e0a30b9ffc090103d73
sjs7049 <sjs7049@gmail.com>
2024-02-23 17:24:28 +0900

    homework 3-3

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
 import pymysql
 
-def high_orders():
+def high_orders_alter():
     connection = pymysql.connect(
         host='localhost', user='root', passwd='worldcup7!',
         database = 'madang',port=3306, charset='utf8')
@@ -8,8 +8,8 @@ def high_orders():
     try:
         with connection.cursor() as cursor:
             sql = """
-            CREATE VIEW Highorders
-            AS SELECT b.bookid, b.bookname, cs.name, b.publisher, od.saleprice
+            CREATE OR REPLACE VIEW Highorders
+            AS SELECT b.bookid, b.bookname, cs.name, b.publisher
             FROM Book b, Customer cs, Orders od
             WHERE cs.custid=od.custid AND b.bookid=od.bookid AND od.saleprice = 20000;
             """
@@ -20,11 +20,11 @@ def high_orders():
             data = cursor.fetchall()
             
             for rowdata in data:
-                print('{0}\t{1:<}\t{2:<}\t{3:<}\t{4:<}'.format(
-                    rowdata[0],rowdata[1],rowdata[2],rowdata[3],rowdata[4]))
+                print('{0}\t{1:<}\t{2:<}\t{3:<}'.format(
+                    rowdata[0],rowdata[1],rowdata[2],rowdata[3]))
             cursor.close()
     finally:
         connection.close()
 
 if __name__ == "__main__":
-    high_orders()
+    high_orders_alter()
