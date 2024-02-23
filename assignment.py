9ec36fd285b5267df7290d6e41baf7bdb535c8b2
sjs7049 <sjs7049@gmail.com>
2024-02-23 17:33:35 +0900

    homework 3 final

--- assignment.py
+++ assignment.py
@@ -1,5 +1,34 @@
 import pymysql
 
+def high_orders():
+    connection = pymysql.connect(
+        host='localhost', user='root', passwd='worldcup7!',
+        database = 'madang',port=3306, charset='utf8')
+    
+    try:
+        with connection.cursor() as cursor:
+            sql = """
+            CREATE VIEW Highorders
+            AS SELECT b.bookid, b.bookname, cs.name, b.publisher, od.saleprice
+            FROM Book b, Customer cs, Orders od
+            WHERE cs.custid=od.custid AND b.bookid=od.bookid AND 
+	    od.saleprice = 20000;
+            """
+            cursor.execute(sql)
+            
+            result = "SELECT * FROM Highorders;"
+            cursor.execute(result)
+            data = cursor.fetchall()
+            
+            for rowdata in data:
+                print('{0}\t{1:<}\t{2:<}\t{3:<}\t{4:<}'.format(
+                    rowdata[0],rowdata[1],rowdata[2],rowdata[3],rowdata[4]))
+            cursor.close()
+    finally:
+        connection.close()
+
+
+
 def high_orders_alter():
     connection = pymysql.connect(
         host='localhost', user='root', passwd='worldcup7!',
@@ -11,7 +40,8 @@ def high_orders_alter():
             CREATE OR REPLACE VIEW Highorders
             AS SELECT b.bookid, b.bookname, cs.name, b.publisher
             FROM Book b, Customer cs, Orders od
-            WHERE cs.custid=od.custid AND b.bookid=od.bookid AND od.saleprice = 20000;
+            WHERE cs.custid=od.custid AND b.bookid=od.bookid AND 
+	    od.saleprice = 20000;
             """
             cursor.execute(sql)
             
@@ -27,4 +57,5 @@ def high_orders_alter():
         connection.close()
 
 if __name__ == "__main__":
+    high_orders()
     high_orders_alter()
